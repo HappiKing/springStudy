4542872d0845d3d268fce224fbd70b310a73a91b
haengbok <haengbok.kim@ntoday.kr>
2025-11-05 22:48:43 +0900

    싱글톤 패턴을 이요한 테스트 코드 작성

--- servletStudy/servlet/src/test/java/hello/servlet/ServletApplicationTests.java
+++ servletStudy/servlet/src/test/java/hello/servlet/ServletApplicationTests.java
@@ -1,13 +1,53 @@
 package hello.servlet;
 
+import hello.servlet.domain.member.Member;
+import hello.servlet.domain.member.MemberRepository;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
 class ServletApplicationTests {
 
+	MemberRepository memberRepository = MemberRepository.getInstance();
+
+	@AfterEach
+	void afterEach() {
+		memberRepository.clearStore();
+	}
+
 	@Test
-	void contextLoads() {
+	void save() {
+		// given
+		Member member = new Member("hello", 20);
+
+		// when
+		Member savedMember = memberRepository.save(member);
+
+		// then
+		Member findMember = memberRepository.findById(savedMember.getId());
+		assertThat(savedMember).isEqualTo(findMember);
+
+	}
+
+	@Test
+	void findAll() {
+		// given
+		Member member1 = new Member("rlo_ji", 19);
+		Member member2 = new Member("happy", 25);
+		memberRepository.save(member1);
+		memberRepository.save(member2);
+
+		// when
+		List<Member> result = memberRepository.findAll();
+
+		// then
+		assertThat(result.size()).isEqualTo(2);
+		assertThat(result).contains(member1, member2);
 	}
 
 }
